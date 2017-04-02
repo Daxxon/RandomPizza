c489b70889ea76979e37561143a65e4da800faf3
Daniel Waggoner <D.Waggoner@outlook.com>
2017-04-01 22:02:06 -0400

    outlined program behaviour lattice

--- RandomizePizza.java
+++ RandomizePizza.java
@@ -1,10 +1,31 @@
-class RandomizePizza {
+import java.util.Random;
 
-  public static void main(String args[]) {
+class RandomizePizza{
+
+  public static void main(String args[]){
 
 
 
 
   }
+}
 
+class Pizza {
+  String[] crust = { originalPan, handTossed, thin, stuffed };
+  String[] sauce = { marinara, crushedTomato, garlicParmesan, barbeque, buffalo };
+  String[] meats = { pepperoni, italianSausage, salami, meatball, ham, bacon, chicken, beef, pork };
+  String[] veggies = { mushrooms, spinach, peruvianPeppers, redOnions, olives, bellPeppers, bananaPeppers, pineapple, jalapenoPeppers, romaTomatoes };
+
+  String chooseCrust(){
+
+  }
+  String chooseSauce(){
+
+  }
+  String chooseMeat(){
+
+  }
+  String chooseVeggies(){
+
+  }
 }
